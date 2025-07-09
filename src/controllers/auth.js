fe9d9fdff17a6c20192a77d4abc9370fcccf3d21
Sasha Savenko <sashasavenko3@gmail.com>
2025-07-09 17:03:36 +0300

    password excluded

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -16,16 +16,16 @@ const setupSession = (res, session) => {
 
 export const regesterController = async (req, res) => {
   const data = await authServices.register(req.body);
-  const userData = {
-    _id: data._id,
-    name: data.name,
-    email: data.email,
-    createdAt: data.createdAt
-  };
+  // const userData = {
+  //   _id: data._id,
+  //   name: data.name,
+  //   email: data.email,
+  //   createdAt: data.createdAt
+  // };
   res.status(201).json({
     status: 201,
     message: 'Successfully registered a user!',
-    userData
+    data
   });
 };
 
